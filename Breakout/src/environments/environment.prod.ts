da698801b827bcb81c62e11c26c1a32705a9e08c
Cameron Knight <cjk8zb@mail.umkc.edu>
2019-05-13 19:48:19 -0500

    Prepare for deployment.

--- Breakout/src/environments/environment.prod.ts
+++ Breakout/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD79D_oyq8EM11RAvYgtv9ubZMJch9fyxc',
+    authDomain: 'cs490-breakout.firebaseapp.com',
+    databaseURL: 'https://cs490-breakout.firebaseio.com',
+    projectId: 'cs490-breakout',
+    storageBucket: 'cs490-breakout.appspot.com',
+    messagingSenderId: '570638023404',
+    appId: '1:570638023404:web:72063e32f1648e9f'
+  }
 };
